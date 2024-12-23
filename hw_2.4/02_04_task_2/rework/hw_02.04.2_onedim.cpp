3ed632c0562f45ffe6826382c5d87406d3b67632
Irbis_neto <irbis@netology.com>
2024-12-23 14:01:19 +0300

    added dirs rework

---
+++ hw_2.4/02_04_task_2/rework/hw_02.04.2_onedim.cpp
@@ -0,0 +1,119 @@
+// Задача 2. Сортируем адреса
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Address
+{
+private:
+    string city{};
+    string street{};
+    string house_number{};
+    string apartment_number{};
+
+public:
+    Address(string _city, string _street, string _house_number, string _apartment_number) // конструктор с параметрами
+    {
+        city = _city;
+        street = _street;
+        house_number = _house_number;
+        apartment_number = _apartment_number;
+    }
+
+    Address() {}
+    ~Address() {};
+
+    void set_city(string _city)
+    {
+        city = _city;
+    }
+    void set_street(string _street)
+    {
+        street = _street;
+    }
+    void set_house_number(string _house_number)
+    {
+        house_number = _house_number;
+    }
+    void set_apartment_number(string _apartment_number)
+    {
+        apartment_number = _apartment_number;
+    }
+
+    string get_city()
+    {
+        return city;
+    }
+    string get_outaddress() // сформированная строка для вывода в файл
+    {
+        return city + ", " + street + ", " + house_number + ", " + apartment_number;
+    }
+};
+
+int main()
+{
+    int N{}; // количество адресов
+
+    // чтение из файла
+    ifstream infile("in.txt");
+    if (infile.is_open())
+    {
+        infile >> N;
+        Address *list_address = new Address[N]; // динамический массив из экземпляров класса Address
+        string _city{};
+        string _street{};
+        string _hn{};
+        string _an{};
+
+        for (int i{}; i < N; ++i)
+        {
+            infile >> _city >> _street >> _hn >> _an;
+
+            list_address[i].set_city(_city);
+            list_address[i].set_street(_street);
+            list_address[i].set_house_number(_hn);
+            list_address[i].set_apartment_number(_an);
+        }
+        infile.close();
+
+        // сортировка пузырьком
+        bool swapped = false;
+        do
+        {
+            swapped = false;
+            for (int i = 1; i < N; ++i)
+            {
+                if (list_address[i - 1].get_city() > list_address[i].get_city()) // по городу
+                {
+                    Address tmp = list_address[i - 1];
+                    list_address[i - 1] = list_address[i];
+                    list_address[i] = tmp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+
+        // запись в файл
+        ofstream outfile("out.txt");
+        outfile << N << '\n';
+
+        {
+            for (int i = 0; i < N; ++i) // вывод адресов в отсортированном порядке
+            {
+                outfile << list_address[i].get_outaddress() << '\n';
+            }
+        }
+        outfile.close();
+
+        delete[] list_address; // освобождение  памяти
+    }
+    else
+    {
+        cout << "Не удалось открыть файл! \n";
+    }
+
+    return 0;
+}
